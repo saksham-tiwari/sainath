5eec00cc4693230c41eb1f5ef3886fcbc64d318a
saksham-tiwari <sakshamt234@gmail.com>
2022-10-02 02:10:30 +0530

    responsive nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
-
+import "./Navbar.css"
 const Navbar = () => {
   return (
-    <nav>
+    <>
+<nav>
         <h2>Navbar</h2>
         <ul>
             <li>Movie</li>
             <li>Events</li>
             <li>Contact Us</li>
         </ul>
-
+        <button onClick={()=>{
+          document.querySelector(".hamMenu").classList.toggle("activeHam")
+        }}>
+          <i class="fa fa-bars"></i>
+        </button>
     </nav>
+    <div className='hamMenu'>
+      <ul>
+        <button onClick={()=>{
+          document.querySelector(".hamMenu").classList.toggle("activeHam")
+        }}>&times;</button>
+        <li>Movie</li>
+        <li>Events</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+    </>
+    
   )
 }
 
